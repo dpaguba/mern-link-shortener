05fe51120eae89a2341f5f14dde259c527859fb4
Dmytro Pahuba <dpaguba@gmail.com>
2023-09-14 12:15:36 +0200

    reg route is done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,8 +7,22 @@ const User = require('../models/User')
 const router = Router()
 
 // /api/auth
-router.post('/register', async (req, res) => {
+router.post('/register', [
+    check('email', 'Incorrect email').isEmail(),
+    check('password', "Min. Password length is 6").isLength({
+        min: 6
+    })
+], async (req, res) => {
     try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Invalid data"
+            })
+        }
+
         const { email, password } = req.body
 
         const candidate = await User.findOne({ email: email })
