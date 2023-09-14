938c89599fb23611cfc1a66016f4f3f592d310e8
Dmytro Pahuba <dpaguba@gmail.com>
2023-09-14 11:24:13 +0200

    connected to mongo db

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 // conn package
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose');
 
 // server
 const app = express()
 
 // constants
 const PORT = config.get('port') || 5000
+const MONGO_URI = config.get('mongoUri')
 
 
-app.listen(PORT, ()=>{
-    console.log(`App has been started onn port ${PORT}`);
-})
+async function start(){
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useCreateIndex: true
+        })
+        console.log(`App has been started onn port ${PORT}`)
+    } catch (error) {
+        console.log("Server Error: ", error.message);
+        process.exit(1)
+    }
+}
+
+start()
