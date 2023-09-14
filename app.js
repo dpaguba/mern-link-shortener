ef8f344208f69efd38e49f7d3a878b7bedcb3d56
Dmytro Pahuba <dpaguba@gmail.com>
2023-09-14 10:49:09 +0200

    port moved to config file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // conn package
 const express = require('express')
+const config = require('config')
 
 // server
 const app = express()
 
-// firsrt param is port, second is callback
-// not best practice
-app.listen(5000, ()=>{
-    console.log("App has been started");
+// constants
+const PORT = config.get('port') || 5000
+
+
+app.listen(PORT, ()=>{
+    console.log(`App has been started onn port ${PORT}`);
 })
